5a10ccba4f9341e5e92c1e3fc13904ed419c8d82
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-11-27 21:53:32 -0700

    Update GameScreen.jsx

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -5,28 +5,63 @@ import { Button } from 'react-native';
 import { View, Image, Text } from 'react-native';
 import { StyleSheet } from 'react-native';
 import GameScenarios from '../components/GameScenarios';
+<<<<<<< Updated upstream
+=======
+import hallwayImage from '../assets/img/hallway.jpeg';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+>>>>>>> Stashed changes
 
 function GameScreen({ navigation }) {
     const [currentScenario, setCurrentScenario] = useState(0);
     const scenarios = GameScenarios();
+
+  
 
     const handelChoice = (choiceIndex) => {
         const nextScenario = scenarios[currentScenario].choices[choiceIndex].nextScenario;
         setCurrentScenario(nextScenario);
     };
 
+<<<<<<< Updated upstream
     const currentScenarioData = scenarios[currentScenario];
+=======
+    const CustomButton = ({ title,onPress}) => {
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.buttonText}>{title}</Text>
+        </TouchableOpacity>
+    };
+
+    const currentScenarioData = scenarios.find((scenario) => scenario.id === currentScenario);
+
+    if (!currentScenarioData) {
+        return (
+          <View style={styles.container}>
+            <Text style={styles.Title}>No scenario found</Text>
+          </View>
+        );
+
+      }
+
+>>>>>>> Stashed changes
   return (
     <View style={styles.container}>
       <Text style={styles.Title}>Travelers</Text>
       <Image source={currentScenarioData.image} style={styles.Image} />
       <Text style={styles.question}>{currentScenarioData.question}</Text>
+
+
       {currentScenarioData.choices.map((choice, index) => (
+<<<<<<< Updated upstream
         <>
         <Button
             key={index}
             style={styles.choiceButton}
             Text={choice.text}
+=======
+        <CustomButton
+            key={index}
+            title={choice.text}
+>>>>>>> Stashed changes
             onPress={() => handelChoice(index)}
         />
         <Button
@@ -88,15 +123,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginVertical: 20,
     },
-    choiceButton: {
-        backgroundColor: 'rgba(39, 39, 39, 1)',
-        width: 200,
+    button: {
+        backgroundColor: 'rgba(90, 90, 90, 1)',
+        width: 310,
         marginVertical: 10,
         marginHorizontal: 50,
-        titleStyle: {
-        color: 'white',
-        marginHorizontal: 20,
-        },
     },
 });
 
