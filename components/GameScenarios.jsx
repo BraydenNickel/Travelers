5a17e05cb83d868191a406a7c6f38ef3ca5575a3
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-11-28 22:17:37 -0700

    Update GameScenarios.jsx

--- components/GameScenarios.jsx
+++ components/GameScenarios.jsx
@@ -1,86 +1,92 @@
 /* eslint-disable prettier/prettier */
-import goblinImage from '../assets/img/enemy_encounter.jpeg';
-import { Image } from '@rneui/base';
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import hallwayImage from '../assets/img/hallway.jpeg';
-import PlayerStats from '../components/PlayerStats';
+import goblinImage from '../assets/img/enemy_encounter.jpeg';
+import forestImage from '../assets/img/forest.jpeg';
+import bullImage from '../assets/img/boss_encounter.jpeg';
+import trinketRoom from '../assets/img/trinket-room.jpeg';
+import { Image } from '@rneui/base';
 
+export default function gameScenarios() {
+  const getRandomInt = ( min, max ) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-export default function GameScenarios() {
-  const [playerStats, setPlayerStats] = useState({
-    health: 100,
-    mana: 50,
-    strength: 5,
-    intelligence: 5,
-    experience: 0,
-    level: 1,
-  });
+  const generateEncounter = ( chance ) => {
+    return getRandomInt(1, 100) <= chance;
+  };
 
-  const updateStats = useCallback((action) => {
-    setPlayerStats((prevStats) => {
-      const updatedStats = {
-        ...prevStats,
-        ...action,
-      };
+  const scenarios = [{
+    id: 'EncounterGoblin', // goblin fight
+    image: Image.resolveAssetSource(goblinImage).uri,
+    question: 'As you walk along, you notice that you start seeing a lot of broken objects. You suddenly hear maniacal laughter approach you. Out of the dim hallway, a goblin appears.',
+    choices: [
+      {id: 'A', text: 'Attack', nextScenario: 'CombatGoblin'},
+      {id: 'B', text: 'Run', nextScenario: 1},
+      {id: 'C', text: 'Test3', nextScenario: 8},
+      {id: 'D', text: 'Turn Back', nextScenario: 'MainHallway'},
+    ],
+  }, {
+    id: 'BeginGame', //First Scene
+    image: Image.resolveAssetSource(forestImage).uri,
+    question: 'You are a lone traveler. Upon hearing tales about a dungeon hidden deep in a forest, you decide to head out and look at it yourself. With nothing more than your trusty sword and and your courage with you, you set out and prepare to enter the dungeon.',
+    choices: [
+      {id: 'A', text: 'Enter the dungeon', nextScenario: 'MainHallwayStart'},
+    ],
+  }, {
+  id: 'MainHallwayStart', 
+  image: Image.resolveAssetSource(hallwayImage).uri,
+  question: 'You find yourself in a well lit hallway with many branching paths. Looks like you have to pick a path to start exploring the dungeon.' ,
+  choices: [
+    {id: 'A', text: 'Go deeper', nextScenario: generateEncounter(40) ? 'EncounterGoblin' : generateEncounter(15) ? 'BossEncounter' : generateEncounter(10) ? 'TrinketRoom' : generateEncounter(5) ? 'TreasureRoom' : 'MainHallway'},
+    {id: 'B', text: 'Go left', nextScenario: generateEncounter(40) ? 'EncounterGoblin' : generateEncounter(15) ? 'BossEncounter' : generateEncounter(10) ? 'TrinketRoom' : generateEncounter(5) ? 'TreasureRoom' : 'MainHallway'},
+    {id: 'C', text: 'Go right', nextScenario: generateEncounter(40) ? 'EncounterGoblin' : generateEncounter(15) ? 'BossEncounter' : generateEncounter(10) ? 'TrinketRoom' : generateEncounter(5) ? 'TreasureRoom' : 'MainHallway'},
+  ],
 
-      console.log('Updated Stats:', updatedStats);
+  // Add more scenarios as needed
+}, {
+  id: 'MainHallway', 
+  image: Image.resolveAssetSource(hallwayImage).uri,
+  question: 'You go further inside the dungeon. You are alone with your thoughts. The path keeps going further.' ,
+  choices: [
+    {id: 'A', text: 'Go deeper', nextScenario: generateEncounter(40) ? 'EncounterGoblin' : generateEncounter(15) ? 'BossEncounter' : generateEncounter(10) ? 'TrinketRoom' : generateEncounter(5) ? 'TreasureRoom' : 'MainHallway'},
+    {id: 'B', text: 'Go left', nextScenario: generateEncounter(40) ? 'EncounterGoblin' : generateEncounter(15) ? 'BossEncounter' : generateEncounter(10) ? 'TrinketRoom' : generateEncounter(5) ? 'TreasureRoom' : 'MainHallway'},
+    {id: 'C', text: 'Go right', nextScenario: generateEncounter(40) ? 'EncounterGoblin' : generateEncounter(15) ? 'BossEncounter' : generateEncounter(10) ? 'TrinketRoom' : generateEncounter(5) ? 'TreasureRoom' : 'MainHallway'},
+  ],
 
-      return updatedStats;
-    });
-  }, []);
+  // Add more scenarios as needed
+},{
+  id: 'TreasureRoom', //Stat Increase
+  image: Image.resolveAssetSource(forestImage).uri,
+  question: 'After who knows how long, you finally reach the fabled treasure room. You enter and feel a burst of happiness. You finally reached the end of your journey.',
+  choices: [
+    {id: 'A', text: 'Continue', nextScenario: 'YouWin'},
+  ],
+}, {
+  id: 'BossEncounter', 
+  image: Image.resolveAssetSource(bullImage).uri,
+  question: 'You enter a well lit hallway. You feel a heavy pressure in the air. A loud roar suddenly assaults your hearing. A Minotaur appears and blocks your path.',
+  choices: [
+    {id: 'A', text: 'Attack', nextScenario: 'CombatGoblin'},
+    {id: 'B', text: 'Run', nextScenario: 1},
+    {id: 'C', text: 'Test3', nextScenario: 8},
+    {id: 'D', text: 'Turn Back', nextScenario: 1},
+  ],
+}, {
+  id: 'TrinketRoom', 
+  image: Image.resolveAssetSource(trinketRoom).uri,
+  question: 'You enter a room full of trinkets. You feel yourself grow stronger whenever you approach one of them. You can only carry one out with you.',
+  choices: [
+    {id: 'A', text: 'HP Ring', nextScenario: 'MainHallway'},
+    {id: 'B', text: 'Mana Necklace', nextScenario: 'MainHallway'},
+    {id: 'C', text: 'Crown of Intelligence', nextScenario: 'MainHallway'},
+    {id: 'D', text: 'Armband of Strength', nextScenario: 'MainHallway'},
+  ],
+},
 
-
-  const scenarios = [
-    {
-      id: 1,
-      image: Image.resolveAssetSource(hallwayImage).uri,
-      question: 'What do you do next?',
-      choices: [
-        { id: 'A', text: 'Explore the dark cave', action: () => updateStats({ intelligence: playerStats.intelligence + 5}), nextScenario: 2 },
-        { id: 'B', text: 'Head towards the village', nextScenario: 3 },
-        { id: 'C', text: 'Go back to the ship', nextScenario: 4 },
-        { id: 'D', text: 'Go back to the ship', nextScenario: 5 },
-      ],
-    },
-    {
-      id: 2,
-      image: Image.resolveAssetSource(hallwayImage).uri,
-      question: 'What do you do next?',
-      choices: [
-        { id: 'A', text: 'Go deeper in the cave', action: () => updateStats({ intelligence: playerStats.intelligence + 5}), nextScenario: 'EncounterGoblin' },
-        { id: 'B', text: 'Test2', nextScenario: 7 },
-        { id: 'C', text: 'Test3', nextScenario: 8 },
-        { id: 'D', text: 'Turn Back', nextScenario: 1 },
-      ],
-    },
-    {
-      id: 'EncounterGoblin', // goblin fight
-      image: Image.resolveAssetSource(hallwayImage).uri,
-      question: 'A goblin appears!',
-      choices: [
-        { id: 'A', text: 'Attack', nextScenario: 'CombatGoblin' },
-        { id: 'B', text: 'Run', nextScenario: 1 },
-        { id: 'C', text: 'Test3', nextScenario: 8 },
-        { id: 'D', text: 'Turn Back', nextScenario: 1 },
-      ],
-    },
-    {
-      id: 'Victory',
-      image: Image.resolveAssetSource(hallwayImage).uri,
-      question: 'You win!',
-      choices: [
-        { id: 'A', text: 'Continue', nextScenario: 1 },
-        { id: 'B', text: 'Test2', nextScenario: 7 },
-        { id: 'C', text: 'Test3', nextScenario: 8 },
-        { id: 'D', text: 'Turn Back', nextScenario: 1 },
-      ],
-    },
-    // Add more scenarios as needed
-  ];
-
-  return { scenarios, updateStats, playerStats };
-}
-
+];
+  return scenarios;
+  }
  /* return {
     id: 1,
     image: Image.resolveAssetSource(hallwayImage).uri,
@@ -93,4 +99,4 @@ export default function GameScenarios() {
     ],
     // Add more scenarios as needed
   };
-} */
+}*/
