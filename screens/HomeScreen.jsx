8324c423c93e124314c8b262413653bca21c6991
EthanCoutts17 <ethankcoutts17@gmail.com>
2023-11-29 20:28:32 -0700

    Animated HomeScreen buttons

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View, Image, Text } from 'react-native';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text, StyleSheet, TouchableOpacity, Animated } from 'react-native';
 import logoTextImage from '../assets/img/logo.png';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
-
-//import HomeLayout from '../layout/HomeLayout';
+//import HomeLayout from '../layout/HomeLayout'; 
 
 
 const HomeScreen = ({ navigation }) => {
+  const [scaleNewGame] = useState(new Animated.Value(1));
+  const [scaleLoadGame] = useState(new Animated.Value(1));
+
   const startNewGame = () => {
     navigation.navigate('IntroPage');
     console.log('New Game started');
@@ -20,18 +20,55 @@ const HomeScreen = ({ navigation }) => {
     console.log('Load Game');
   };
 
+  const breatheAnimation = (scale) => {
+    return Animated.sequence([
+      Animated.timing(scale, {
+        toValue: 0.9,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scale, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+    ]);
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const runAnimation = () => {
+      if (isMounted) {
+        Animated.loop(breatheAnimation(scaleNewGame)).start();
+        Animated.loop(breatheAnimation(scaleLoadGame)).start();
+      }
+    };
+
+    runAnimation();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [scaleNewGame, scaleLoadGame]);
 
   return (
     <View style={styles.container}>
       <Image source={logoTextImage} style={styles.Image} />
       <View style={styles.ButtonContainer}>
         <Text style={styles.Title}>TRAVELERS</Text>
-        <TouchableOpacity style={styles.button} onPress={startNewGame}>
-            <Text style={styles.buttonText}>New Game</Text>
+        <TouchableOpacity
+          style={[styles.button, { transform: [{ scale: scaleNewGame }] }]}
+          onPress={startNewGame}
+        >
+          <Text style={styles.buttonText}>New Game</Text>
         </TouchableOpacity>
         <View style={styles.ButtonSpace} />
-        <TouchableOpacity style={styles.button} onPress={loadSavedGame}>
-            <Text style={styles.buttonText}>Load Game</Text>
+        <TouchableOpacity
+          style={[styles.button, { transform: [{ scale: scaleLoadGame }] }]}
+          onPress={loadSavedGame}
+        >
+          <Text style={styles.buttonText}>Load Game</Text>
         </TouchableOpacity>
       </View>
     </View>
